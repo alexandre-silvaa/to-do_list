2c3ae8f9a26faee970aeced3f58593211536bf54
alexandre-silvaa <alexandrebsilva9@gmail.com>
2025-09-01 20:28:58 -0300

    fix: remove onAddTask prop from Header component

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -4,11 +4,10 @@ import type { AddTaskModalRef } from "@/@types/tasks.type";
 import type { RefObject } from "react";
 
 type HeaderProps = {
-  readonly onAddTask: (taskName: string) => void;
   readonly modalRef: RefObject<AddTaskModalRef | null>;
 };
 
-export function Header({ onAddTask, modalRef }: Readonly<HeaderProps>) {
+export function Header({ modalRef }: Readonly<HeaderProps>) {
   return (
     <div className="flex flex-row w-full justify-between gap-[32px] items-center">
       <span className="font-mono text-4xl">ToDo List</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Header onAddTask={onAddTask} modalRef={modalRef} />
+        <Header modalRef={modalRef} />
         {loading ? (
           <p className="text-gray-500">Carregando tarefas...</p>
         ) : (
